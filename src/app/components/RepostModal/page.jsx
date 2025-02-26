5f1c13ce6548e5464d2d2856a2f03c7b40c700cd
InternPromi <promi.shellbeehaken@gmail.com>
2025-02-26 12:23:59 +0600

    handle modal position empty case

--- src/app/components/RepostModal/page.jsx
+++ src/app/components/RepostModal/page.jsx
@@ -3,18 +3,23 @@
 import { Repeat, Quote } from "lucide-react";
 import styles from "./page.module.css"; 
 
-const RepostModal = ({ modalPosition, onClose, onRepost, isReposted, onQuote }) => {
+const RepostModal = ({ modalPosition = { top: 0, left: 0 }, onClose, onRepost, isReposted, onQuote }) => {
+  if (!modalPosition) return null; // Prevent error if modalPosition is undefined
+
   return (
     <div
       className={styles.modal}
-      style={{ top: `${modalPosition.top}px`, left: `${modalPosition.left}px` }}
+      style={{ 
+        top: `${modalPosition?.top ?? 0}px`, 
+        left: `${modalPosition?.left ?? 0}px`
+      }}
       onClick={(e) => {
         e.stopPropagation();
         onClose();
       }}
     >
       <div className={styles.modalButton} onClick={onRepost}>
-        <Repeat size={15} /> {isReposted? "Undo repost": "Repost"}
+        <Repeat size={15} /> {isReposted ? "Undo repost" : "Repost"}
       </div>
       <div className={styles.modalButton} onClick={onQuote}>
         <Quote size={15} /> Quote
